ce8db6171915e8ba117a045b2eb94bff94442c5e
Masud <masud.pce@gmail.com>
2021-02-07 01:02:59 +0600

    Add comment of where trackbar is initialized

--- 1-histogram/2_histogram_lane_pi_murtaza/LaneDetectionModule.py
+++ 1-histogram/2_histogram_lane_pi_murtaza/LaneDetectionModule.py
@@ -21,11 +21,12 @@ def getLaneCurve(img, display=2):
     """ Produce mask; min and max hsv value range is hardcoded in utlis module."""
     imgThres = utlis.thresholding(img)
 
-    points = utlis.valTrackbars()
     # ============ STEP 2
     """Region of interest will be warped and resized
      to the size of original image"""
     hT, wT, c = img.shape     # the order is Height, width and channel/depth
+    points = utlis.valTrackbars()      # trackbar initialized with values in main function
+
     imgWarp = utlis.warpImg(imgThres, points, wT, hT)
     imgWarpPoints = utlis.drawPoints(imgCopy, points)
 
