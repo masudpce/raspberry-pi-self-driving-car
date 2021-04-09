c600462387ff3a3e2ca2ccc876474145e6ab6f09
masudpce <masud.pce@gmail.com>
2021-04-09 15:40:51 +0600

    Create remote control codes

---
+++ 1_histogram/2_histogram_lane_pi_murtaza/murtaza_pi_version/remote_control_car/main_robot_remote.py
@@ -0,0 +1,19 @@
+import key_press_module as kp
+from gpiozero import Robot
+
+robot = Robot(left=(4,14), right=(17, 18))
+
+kp.init()
+
+def main():
+    if kp.getKey('UP'):
+        robot.forward(1)
+    elif kp.getKey('DOWN'):
+        robot.backward(1)
+    elif kp.getKey('LEFT'):
+        robot.left(0.5)
+    elif kp.getKey('RIGHT'):
+        robot.right(0.5)
+    else:
+        robot.stop()
+        
